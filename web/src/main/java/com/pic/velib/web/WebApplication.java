3bb6a13ad2b138064de687c34c42a272eecb2852
Kevin Patinaud <you@example.com>
2023-01-03 19:00:03 +0100

    https

--- web/src/main/java/com/pic/velib/web/WebApplication.java
+++ web/src/main/java/com/pic/velib/web/WebApplication.java
@@ -7,17 +7,25 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Collections;
+import java.util.Map;
 
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class WebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(WebApplication.class);
-	//	app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
-		app.run(args);
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(WebApplication.class);
 
-	//	SpringApplication.run(WebApplication.class, args);
-	}
+        Map defaultProperties = Collections.singletonMap("server.port", "8083");
+        defaultProperties.put("server.ssl.enabled", "true");
+        defaultProperties.put("server.ssl.keyAlias", "tomcat");
+        defaultProperties.put("server.ssl.key-store", "/etc/letsencrypt/live/velib-forecast.com/keystore.p12");
+        defaultProperties.put("server.ssl.key-store-password", "Hack4Ever!");
+        defaultProperties.put("server.ssl.keyStoreType", "PKCS12");
+        app.setDefaultProperties(defaultProperties);
+
+        app.run(args);
+
+    }
 
 }
