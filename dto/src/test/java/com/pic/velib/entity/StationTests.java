b9bbfc2dd9c8a9120e2c2a0167dc36f036f8f463
Kevin Patinaud <you@example.com>
2023-01-19 09:06:52 +0100

    feat tests

--- dto/src/test/java/com/pic/velib/entity/StationTests.java
+++ dto/src/test/java/com/pic/velib/entity/StationTests.java
@@ -1,18 +1,14 @@
 package com.pic.velib.entity;
 
-import com.pic.velib.repository.StationRepository;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class StationTest {
-
+public class StationTests {
 
     @Test
     public void toJSON() {
---
+++ service/src/test/java/com/pic/velib/service/api/ApiTests.java
@@ -0,0 +1,14 @@
+package com.pic.velib.service.api;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApiTests {
+
+    @Test
+    public void sampleTest()
+    {
+        assertThat(true);
+    }
+}
